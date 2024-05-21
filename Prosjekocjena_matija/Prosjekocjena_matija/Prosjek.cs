8bf34c04deabdf239887aa16f38804fb42abaf0e
matijaopacak06 <153612293+matijaopacak06@users.noreply.github.com>
2024-05-21 12:21:29 +0200

    Add files via upload

---
+++ Prosjekocjena_matija/Prosjekocjena_matija/Prosjek.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prosjekocjena_matija
+{
+    internal class Prosjek
+    {
+        string nazivPredmeta;
+        int ocjena;
+
+        
+
+        public Prosjek(string nazivPredmeta, int ocjena)
+        {
+            this.NazivPredmeta = nazivPredmeta;
+            this.Ocjena = ocjena;
+        }
+        public Prosjek()
+        {
+          
+        }
+        public string NazivPredmeta { get => nazivPredmeta; set => nazivPredmeta = value; }
+        public int Ocjena { get => ocjena; set => ocjena = value; }
+    }
+
+}
